60209b26d385c53c7938456e18ac0227b7363439
FernanMag <157384629+FernanMag@users.noreply.github.com>
2024-02-10 20:07:32 +1000

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,14 @@
 swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     loop: true,
+    spaceBetween: 30,
     pagination: {
         el: '.swiper-pagination',
         type: 'fraction',
     },
     navigation: {
-        nextEl: '.swiper-container .swiper-button-next',
-        prevEl: '.swiper-container .swiper-button-prev'},
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'},
     scrollbar: {
         el: '.swiper-scrollbar',
         draggable: true
